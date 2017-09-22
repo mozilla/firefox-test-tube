2b0c80541e0e351db243dc30a3384dedc6643040
John Karahalis <john.karahalis@gmail.com>
2017-09-22 19:34:21 -0400

    Only show loading spinner after 500 ms

--- src/components/views/Loading.js
+++ src/components/views/Loading.js
@@ -4,8 +4,28 @@ import { Icon } from 'react-fa';
 import './css/Loading.css';
 
 
-export default () => (
-    <div id="loading">
-        <Icon spin name="circle-o-notch" />
-    </div>
-);
+export default class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { component: null };
+        this.wait = 500;
+        this.loadingComponent = (
+            <div id="loading">
+                <Icon spin name="circle-o-notch" />
+            </div>
+        );
+    }
+
+    /**
+     * Only show the spinner after this.wait milliseconds.
+     */
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ component: this.loadingComponent });
+        }, this.wait);
+    }
+
+    render() {
+        return this.state.component;
+    }
+}
