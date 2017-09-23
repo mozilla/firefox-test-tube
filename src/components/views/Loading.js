e8b4f7fd1dbfab9532237fb04b7420dd2f45fd52
John Karahalis <john.karahalis@gmail.com>
2017-09-23 01:52:04 -0400

    Refactor the loading spinner a bit

--- src/components/views/Loading.js
+++ src/components/views/Loading.js
@@ -7,25 +7,27 @@ import './css/Loading.css';
 export default class extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { component: null };
-        this.wait = 500;
-        this.loadingComponent = (
-            <div id="loading">
-                <Icon spin name="circle-o-notch" />
-            </div>
-        );
+        this.state = { showSpinner: false };
     }
 
     /**
-     * Only show the spinner after this.wait milliseconds.
+     * Only show the spinner after the given number of milliseconds.
      */
     componentDidMount() {
         setTimeout(() => {
-            this.setState({ component: this.loadingComponent });
-        }, this.wait);
+            this.setState({ showSpinner: true });
+        }, process.env.REACT_APP_SPINNER_WAIT);
     }
 
     render() {
-        return this.state.component;
+        if (this.state.showSpinner) {
+            return (
+                <div id="loading">
+                    <Icon spin name="circle-o-notch" />
+                </div>
+            );
+        }
+
+        return null;
     }
 }
