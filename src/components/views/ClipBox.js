1f8b4ab472c08df31b437a6273012a37ade3f72b
Davor Spasovski <davor@spasovski.net>
2018-08-23 16:16:32 -0500

    add copy slug to clipboard component

---
+++ src/components/views/ClipBox.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import './css/ClipBox.css';
+
+
+class ClipBox extends Component {
+    constructor(props) {
+        super(props);
+
+        this.inputRef = React.createRef();
+    }
+
+    handleInputFocus = e => {
+        if (e.target) e.target.select();
+    }
+
+    handleCopyClick = e => {
+        if (!e.target) return;
+        e.target.parentNode.querySelector('input').select();
+        document.execCommand('copy');
+    }
+
+    render() {
+        return (
+            <div className="clipbox">
+                <label>
+                    {this.props.label}
+                    <input type="text" onFocus={this.handleInputFocus} readOnly value={this.props.textValue} />
+                    <a onClick={this.handleCopyClick} className="btn-copy">copy to clipboard</a>
+                </label>
+            </div>
+        );
+    }
+}
+
+export default ClipBox;
