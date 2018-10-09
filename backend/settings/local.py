c1a34f007b249f4913b8ac36b4ce5009b48c5725
Davor Spasovski <davor@spasovski.net>
2018-10-09 16:37:47 -0500

    add option to disable auth0 for local dev

--- backend/settings/local.py
+++ backend/settings/local.py
@@ -2,3 +2,5 @@ from .base import *  # noqa
 
 
 MIDDLEWARE.append('backend.middleware.dev_cors_middleware')  # noqa
+# Uncomment the following to disable Auth0 on the Django Rest Framework API endpoints.
+#REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] = ('rest_framework.permissions.AllowAny',) # noqa
