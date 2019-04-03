fac888be2674ec776eb7f78fda1e3391827b5e80
Rob Hudson <robhudson@mozilla.com>
2019-04-03 08:26:21 -0700

    Upgrade to Django 2.2

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -39,7 +39,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     # Django
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
